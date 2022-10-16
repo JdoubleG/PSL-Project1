a3511d2c5db18adc1a831c7af0b657a5444487a1
J. Garijo-Garde <jamesig2@illinois.edu>
2022-10-15 22:28:10 -0400

    File stubs for project

---
+++ evaluation.R
@@ -0,0 +1,13 @@
+# Project 1
+#
+# Team: James Garijo-Garde (jamesig2) & Matthew Lind (YOUR_LOGIN)
+
+# Suppose we have already read test_y.csv in as a two-column 
+# data frame named "test.y":
+# col 1: PID
+# col 2: Sale_Price
+
+pred <- read.csv("mysubmission1.txt")
+names(test.y)[2] <- "True_Sale_Price"
+pred <- merge(pred, test.y, by="PID")
+sqrt(mean((log(pred$Sale_Price) - log(pred$True_Sale_Price))^2))
---
+++ mymain.R
@@ -0,0 +1,28 @@
+# Project 1
+#
+# Team: James Garijo-Garde (jamesig2) & Matthew Lind (YOUR_LOGIN)
+
+
+###########################################
+# Step 0: Load necessary libraries
+#
+# YOUR CODE
+# 
+
+###########################################
+# Step 1: Preprocess training data
+#         and fit two models
+#
+train <- read.csv("train.csv")
+#
+# YOUR CODE
+# 
+
+###########################################
+# Step 2: Preprocess test data
+#         and output predictions into two files
+#
+test <- read.csv("test.csv")
+#
+# YOUR CODE
+# 
